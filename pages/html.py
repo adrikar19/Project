a56647dc3cc1ffc1be3dd5cc3b4bb1f192356522
adrikar19 <adrikar19@gmail.com>
2024-11-20 11:24:20 +0000

    Update html ChatAPI

--- pages/html.py
+++ pages/html.py
@@ -1,6 +1,50 @@
 import streamlit as st
+from openai import OpenAI
 
-st.set_page_config(
-    page_title="JAVA Interview Panel",page_icon="🤖"
-)
-st.title(":blue[Welcome to HTML interview panel]")
+# st.set_page_config(
+#     page_title="HR Interview ,page_icon="🤖"
+# )
+#st.title(":blue[Welcome to HR interview screen]")
+
+
+# if "messages" in st.session_state:
+#     del st.session_state["messages"]
+
+st.title("Euphoria's HTML Connect")
+
+# Set OpenAI API key from Streamlit secrets
+client = OpenAI(api_key="sk-Ex2o5lap7eghRuDJs5AAtKJVpAF-CaHm1-567EixP9T3BlbkFJ6egUK5hltqhNohneix8ePZ2uaxmzZgE3DdL63XUzcA")
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4o-mini"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "system", "content": "You are a Mock HTML Technology Interviewer. You need to generate HTML interview questions and validate user's answers. Always ask one question at a time. Always ask one question at a time and if answer is correct ask the next question in the same response. Ask slowly easy to tough questions. If user starts different discussion, bring them back to HTML interview discussion."}]
+    st.session_state.messages.append({"role": "assistant", "content": "Hello, welcome to Euphoria, How are you today?"})
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    if(message["role"] !="system"):
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
